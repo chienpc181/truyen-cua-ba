ec664af8f8c2f5c3abba1c00eb81d9cce590f724
chien pham <chien@rangstrup.com>
2024-08-10 17:25:52 +0700

    fix authen

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./fbServiceAccountKey.json');
+// const serviceAccount = require('./fbServiceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
