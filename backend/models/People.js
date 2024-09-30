2f939bb54f1cea10bc33d72e22ffc9ec3b3bd321
chien pham <chien@rangstrup.com>
2024-09-30 10:23:50 +0700

    Adding new model: People

---
+++ backend/models/People.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const peopleSchema = mongoose.Schema({
+    title: {
+        en: String,
+        vi: String
+    },
+    // author: {
+    //     type: String,
+    //     required: [true, 'Author is required'],
+    // },
+    // genre: {
+    //     type: String,
+    // },
+    // publishedDate: {
+    //     type: Date,
+    // },
+    // introduction: [
+    //     {
+    //         en: String,
+    //         vi: String
+    //     }
+    // ],
+    // paragraphs: [
+    //     {
+    //         en: String,
+    //         vi: String
+    //     }
+    // ],
+    // thumbnailUrl: {
+    //     type: String
+    // }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('People', peopleSchema);
